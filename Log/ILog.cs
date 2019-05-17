3f563e20ebaabc3a737490a8d4163d5ccf93c10e
罗运永 <luoyunyong@hotmail.com>
2019-05-18 00:02:17 +0800

    新增日志接口ILog

---
+++ Log/ILog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YLM.Common.Log
+{
+    /// <summary>
+    /// 新增日志接口
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    interface ILog<T> where T : class
+    {
+        /// <summary>
+        /// 新增日志
+        /// </summary>
+        /// <param name="t"></param>
+        void AddLog(T t);
+
+        /// <summary>
+        /// 查询日志
+        /// </summary>
+        /// <param name="logId"></param>
+        /// <returns></returns>
+        T Get(int logId);
+    }
+}
